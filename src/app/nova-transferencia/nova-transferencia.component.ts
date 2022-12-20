0dfe829f49c7a610e99c784cb3798bc27669d3c8
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2022-12-20 16:03:48 -0300

    FEAT: Colocando eventos do Angular e manipulando variaveis atráves de Data Binding

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,13 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
-    selector:`app-nova-transferencia`,
-    templateUrl:`./nova-transferencia.component.html`,
-    styleUrls:[`./nova-transferencia.component.scss`]
+    selector: `app-nova-transferencia`,
+    templateUrl: `./nova-transferencia.component.html`,
+    styleUrls: [`./nova-transferencia.component.scss`]
 })
 
-export class NovaTransferenciaComponent{
-    transferir(){
+export class NovaTransferenciaComponent {
+
+    @Output() aoTransferir = new EventEmitter<any>();
+
+    valor: number;
+    destino: string;
+
+    transferir() {
         console.log('Solicitada a tranferencia')
+        const valorEmitir = { valor: this.valor, destino: this.destino };
+        this.aoTransferir.emit(valorEmitir);
+        this.limparCampos();
     }
+
+    limparCampos(){
+        this.valor = null;
+        this.destino = ' ' 
+      }
 }
